5fdccf87b8f572d145baf931118756b4f724b4a1
Tim <nebukam@gmail.com>
2023-01-04 20:50:54 -0500

    Update
    Added the ability to run the script only on active selection, and moved it to the Scene panel instead of world.

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # blender-material-replacement ![version](https://img.shields.io/badge/version-1.0.0-darkgreen) ![license](https://img.shields.io/badge/license-MIT-black.svg)
-## A straighforward blender add-on that replaces materials created during import with suffixes (in the form of '.###') with existing materials with the same name and no suffix.
+## A straighforward blender add-on that replaces materials created during import with suffixes (in the form of '.###') with existing materials with the same name and no suffix.  
+  
+Long story short, will swap `MyMatID.001`, `MyMatID.010`, `MyMatID.012` for **`MyMatID`** if it exists, and remove the others. If no "main" material is found, the script simply ignore the material.  
+  
+Support Blender 3.3.x, may work with older version, don't know, haven't tested.
+
 
 --
 ## Install
 Simply download this repo as a .ZIP and follow [blender's instructions](https://docs.blender.org/manual/en/latest/editors/preferences/addons.html) to install the add-on.
 
 ## Use
-Once installed and enabled, a new panel will be available in the world properties under "Material Utils", simply click the button "Replace Material Duplicates" and the script will go through all objects within the scene, replacing duplicate material references with their "parent" version.
+Once installed and enabled, a new panel will be available in the **scene properties tab** under **Material Duplicate Clean-up**, simply click the button `Run on Scene` to clean-up all materials in the scene, or `Run on Selection` to only affect the active selection.  
+  
+*Note that `Run on Selection` does not remove replaced duplicate, as sometimes dupes are used outside of the selection, resulting in loss of data.*
+
+### Limitations
+Running this on the scene loop through all objects, one by one, and for each object loops through all material within the .blend file, one by one. As such, if you have a very very complex scene with either tons of objects, or tons of materials, or both, you are very likely to experience small freezes. It's not ideal, but it works :)
--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 bl_info = {
-    "name": "Material Duplicates Replacement",
+    "name": "Material Replacement",
     "author": "Timothé Lapetite",
     "version": (1, 0),
-    "blender": (3, 3, 0),
+    "blender": (2, 90, 0),
     "location": "Preferences > Add-ons",
     "description": "Replaces materials created during import with suffixes (in the form of '.###') with existing materials with the same name and no suffix.",
     "warning": "",
@@ -15,62 +15,75 @@ bl_info = {
 import bpy
 import re
 
-def replace_material_duplicates():
-    # Get all objects in the scene
-    objects = bpy.data.objects
-
-    # Regular expression pattern to match material duplicates
-    pattern = r"(.+)\.[0-9][0-9][0-9]"
-
-    # Go through all objects
+def replace_material_duplicates(objects, cleanup):
+    duplicates = []
+    
+    # Iterate through the objects
     for obj in objects:
-        # Go through all material slots of the object
-        for slot in obj.material_slots:
-            # Get the material of the slot
-            material = slot.material
+        # Iterate through the materials of the object
+        for mat_slot in obj.material_slots:
+            mat = mat_slot.material
+            # Check if the material name matches the duplicate material pattern
+            if re.match('.*\.\d{3}$', mat.name):
+                # Strip off the ending dot and digits to get the original material name
+                original_name = mat.name[:-4]
+                # Try to get the original material
+                original_mat = bpy.data.materials.get(original_name)
+                if original_mat is not None:
+                    # If the original material exists, set the material slot to use it
+                    mat_slot.material = original_mat
+                    # Add the duplicate material to the list to be deleted later
+                    if mat not in duplicates:
+                        duplicates.append(mat)
+    
+    # Delete all the duplicate materials
+    if cleanup:
+        for mat in duplicates:
+            bpy.data.materials.remove(mat)
 
-            # Check if the material is a duplicate
-            if re.match(pattern, material.name):
-                # Get the name of the original material
-                original_name = re.sub(pattern, r"\1", material.name)
-
-                # Get the original material
-                original_material = bpy.data.materials.get(original_name)
-
-                # Replace the duplicate material with the original material
-                if original_material:
-                    slot.material = original_material
-
-class ReplaceMaterialDuplicates(bpy.types.Operator):
+class FindAndReplaceAll(bpy.types.Operator):
     """Replace All Material Duplicates"""
-    bl_idname = "object.replace_material_duplicates"
-    bl_label = "Replace All Material Duplicates"
+    bl_idname = "object.find_and_replace_in_scene"
+    bl_label = "Run on Scene"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        replace_material_duplicates()
+        replace_material_duplicates(bpy.data.objects, True)
+        return {'FINISHED'}
+
+class FindAndReplaceInSelection(bpy.types.Operator):
+    """Replace Material Duplicates within Selection"""
+    bl_idname = "object.find_and_replace_in_selection"
+    bl_label = "Run on Selection"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        replace_material_duplicates(bpy.context.selected_objects, False) #no cleanup in case removed mats are used outside selection
         return {'FINISHED'}
 
 class MaterialPanel(bpy.types.Panel):
-    bl_label = "Material Utils"
-    bl_idname = "OBJECT_PT_material_utils"
+    bl_label = "Material Duplicates Clean-up"
+    bl_idname = "SCENE_PT_material_utils"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
-    bl_context = "world"
+    bl_context = "scene"
 
     def draw(self, context):
-        layout = self.layout
-        layout.operator("object.replace_material_duplicates")
+        layout = self.layout        
+        # Add a button to apply the function to all objects in the scene
+        layout.operator("object.find_and_replace_in_scene")
+        # Add a button to apply the function to the active selection
+        layout.operator("object.find_and_replace_in_selection")
 
 def register():
-    bpy.utils.register_class(ReplaceMaterialDuplicates)
+    bpy.utils.register_class(FindAndReplaceAll)
+    bpy.utils.register_class(FindAndReplaceInSelection)
     bpy.utils.register_class(MaterialPanel)
-    bpy.app.handlers.depsgraph_update_post.append(replace_material_duplicates)
 
 def unregister():
-    bpy.utils.unregister_class(ReplaceMaterialDuplicates)
+    bpy.utils.unregister_class(FindAndReplaceAll)
+    bpy.utils.unregister_class(FindAndReplaceInSelection)
     bpy.utils.unregister_class(MaterialPanel)
-    bpy.app.handlers.depsgraph_update_post.remove(replace_material_duplicates)
 
 if __name__ == "__main__":
     register()
